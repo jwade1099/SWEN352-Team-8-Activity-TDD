e3d6455596d96f657acc74c2f22fd5d41f1619cb
bobby <robert.falso2@gmail.com>
2025-04-10 15:03:44 -0400

    MyOptional : 1 : empty() factory : create failing test

--- src/test/java/edu/rit/swen352/tdd/MyOptionalTest.java
+++ src/test/java/edu/rit/swen352/tdd/MyOptionalTest.java
@@ -1,10 +1,18 @@
 package edu.rit.swen352.tdd;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test suite for the {@link MyOptional} component.
  */
 class MyOptionalTest {
+    @Test
+    void emptyReturnsEmptyOptional() {
+        MyOptional<String> optional = MyOptional.empty();
+
+        assertFalse(optional.isPresent());
+    }
 
 }
