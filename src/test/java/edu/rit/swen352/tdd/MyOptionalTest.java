26aa5a5b26d1ef98d7f83dfb93e55f260f8dde7e
bobby <robert.falso2@gmail.com>
2025-04-10 16:04:42 -0400

    MyOptional : 2 : of method : create failing test cases

--- src/test/java/edu/rit/swen352/tdd/MyOptionalTest.java
+++ src/test/java/edu/rit/swen352/tdd/MyOptionalTest.java
@@ -1,5 +1,6 @@
 package edu.rit.swen352.tdd;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +14,20 @@ class MyOptionalTest {
         MyOptional<String> optional = MyOptional.empty();
 
         assertFalse(optional.isPresent());
+    }
+
+    @DisplayName("Creating an optional with a non-null value")
+    @Test
+    void ofNonNull() {
+        MyOptional<String> optional = MyOptional.of("hello");
+
+        assertTrue(optional.isPresent());
+    }
+
+    @DisplayName("Creation of a null value in an optional should through a NPE")
+    @Test
+    void ofNull() {
+        assertThrows(NullPointerException.class, () -> MyOptional.of(null));
     }
 
 }
