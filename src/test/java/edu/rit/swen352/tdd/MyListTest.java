439a083c51d0d23cb00689385b971924e4e2baa9
Aidan Call <acc9123@rit.edu>
2025-04-13 16:28:16 -0400

    MyList : 1 : constructor : create failing test

--- src/test/java/edu/rit/swen352/tdd/MyListTest.java
+++ src/test/java/edu/rit/swen352/tdd/MyListTest.java
@@ -2,9 +2,14 @@ package edu.rit.swen352.tdd;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Test suite for the {@link MyList} component.
  */
 class MyListTest {
-
+    @Test
+    void constructor(){
+        assertDoesNotThrow(() -> new MyList<String>("First", "Second", "Third"));
+    }
 }
