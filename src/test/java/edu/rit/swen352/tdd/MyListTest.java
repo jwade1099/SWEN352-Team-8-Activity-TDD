3a0fea7848ecbd2f027dbac19fd85e938b535854
Aidan Call <acc9123@rit.edu>
2025-04-13 16:38:15 -0400

    MyList : 2 : get : create failing tests

--- src/test/java/edu/rit/swen352/tdd/MyListTest.java
+++ src/test/java/edu/rit/swen352/tdd/MyListTest.java
@@ -12,4 +12,16 @@ class MyListTest {
     void constructor(){
         assertDoesNotThrow(() -> new MyList<String>("First", "Second", "Third"));
     }
+
+    @Test
+    void getWorking(){
+        MyList<String> list = new MyList<String>("First", "Second", "Third");
+        assertEquals("Second", list.get(1));
+    }
+
+    @Test
+    void getError(){
+        MyList<String> list = new MyList<String>("First", "Second", "Third");
+        assertThrows(java.util.NoSuchElementException.class, () -> list.get(5));
+    }
 }
