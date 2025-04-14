40070610584f43ce1d3d7357c516107e8021667b
Justin <jpwade109@gmail.com>
2025-04-14 00:30:58 -0400

    MyBank : 1 : constructor : create failing test

---
+++ src/main/java/edu/rit/swen352/tdd/MyBank.java
@@ -0,0 +1,31 @@
+package edu.rit.swen352.tdd;
+
+
+
+
+
+
+/**
+ *
+ *
+ *My Bank
+ *   <li>constructor: deafults account to 0$
+ *   <li> depost: deposit money into an account. use a boolean to decide if it is savings or checking
+ *   <li> withdraw: take money out of an account
+ *   <li> Transfer: transfer money between accounts
+ *   
+ * </ul>
+ *
+ * @param <T> the type of elements in the stack.
+ */
+
+public class MyBank {
+    private float checking;
+    private float savings;
+
+    public MyBank() throws Exception {
+        throw new Exception();
+
+    }
+    
+}
---
+++ src/test/java/edu/rit/swen352/tdd/MyBankTest.java
@@ -0,0 +1,22 @@
+package edu.rit.swen352.tdd;
+
+
+import edu.rit.swen352.tdd.MyBank;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test suite for the {@link MyStack} component.
+ */
+
+
+
+public class MyBankTest {
+
+
+    @Test
+    void testConstructor() throws Exception {
+        MyBank bank = new MyBank();
+        
+    }
+}
